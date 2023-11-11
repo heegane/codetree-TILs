f8c7fded4b43189614d9b6141bf7133dc061ff31
Hyeon Hee <101989268+heegane@users.noreply.github.com>
2023-11-11 23:29:15 +0900

    Title: 나이트, Time: 154ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231111/나이트/knight-movements.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+n = int(input())
+
+dxs = [-1, -2, -2, -1, 1, 2, 2, 1]
+dys = [-2, -1, 1, 2, -2, -1, 1, 2]
+
+visited = [[0] * n for _ in range(n)]
+step = [[0] * n for _ in range(n)]
+
+
+def bfs(x, y):
+    visited[x][y] = 1
+    step[x][y] = 0
+    q = deque()
+    q.append([x, y])
+
+    while q:
+        x, y = q.popleft()
+        for i in range(8):
+            next_x = x + dxs[i]
+            next_y = y + dys[i]
+            if 0 <= next_x < n and 0 <= next_y < n:
+                if visited[next_x][next_y] == 0:
+                    visited[next_x][next_y] = 1
+                    step[next_x][next_y] = step[x][y] + 1
+                    q.append([next_x, next_y])
+
+
+r1, c1, r2, c2 = map(int, input().split())
+bfs(r1 - 1, c1 - 1)
+if step[r2 - 1][c2 - 1] == 0:
+    print(-1)
+else:
+    print(step[r2 - 1][c2 - 1])
