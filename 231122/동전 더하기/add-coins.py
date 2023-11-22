875e9742077df014ff6e4d9b2b1669d615a7b6e5
Hyeon Hee <101989268+heegane@users.noreply.github.com>
2023-11-22 21:58:26 +0900

    Title: 동전 더하기, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231122/동전 더하기/add-coins.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+coins = []
+for _ in range(n):
+    coins.append(int(input()))
+coins.sort(reverse=True)
+cnt = 0
+for coin in coins:
+    if k < coin:
+        continue
+    else:
+        cnt += k // coin
+        k = k % coin
+print(cnt)
