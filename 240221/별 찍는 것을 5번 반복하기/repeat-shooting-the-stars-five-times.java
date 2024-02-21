b3daa65eafa6886d42d54cc559043866c2fc52ae
Hyeonhee Lee <dlgusgml1314@naver.com>
2024-02-21 20:46:12 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240221/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    public static void print10Stars() {
+        System.out.println("**********");
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i<5; i++) {
+            printStars();
+        }
+    }
+}
