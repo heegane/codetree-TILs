1c4610054f27b8d931365448521d46de3a14e0eb
Hyeonhee Lee <dlgusgml1314@naver.com>
2024-02-21 21:21:09 +0900

    Title: 숫자로 이루어진 사각형, Time: 222ms, Memory: 12MB, Status: Passed - CodeTree

--- 240221/숫자로 이루어진 사각형/rectangle-with-a-number.java
+++ 240221/숫자로 이루어진 사각형/rectangle-with-a-number.java
@@ -5,6 +5,8 @@ public class Main {
 		int num = 1;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
+				if (num == 10)
+					num = 1;
 				System.out.print(num + " ");
 				num++;
 			}
