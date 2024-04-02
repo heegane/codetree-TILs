4456f0859f7169b489d7acf123415878a404918d
Lee Hyeonhee <dlgusgml1314@naver.com>
2024-04-02 16:11:25 +0900

    Title: 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240402/사각형 채우기/rectangle-fill.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [1, 2, 3, 5]
+
+if n <= 4:
+    print(dp[n-1]%10007)
+else:
+    for i in range(4,n-4):
+        dp.append(dp[n-2]+dp[n-1])
+    print(dp[n-1]%10007)
