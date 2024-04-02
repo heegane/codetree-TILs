a819e8945fa6b280fbad11b3a6a2c20b641c1642
Lee Hyeonhee <dlgusgml1314@naver.com>
2024-04-02 16:27:18 +0900

    Title: 사각형 채우기 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240402/사각형 채우기 2/rectangle-fill-2.py
+++ 240402/사각형 채우기 2/rectangle-fill-2.py
@@ -6,5 +6,8 @@ if n <= 3:
     print(dp[n-1]%10007)
 else:
     for i in range(3,n+1):
-        dp.append(2*dp[i-1]-1)
+        if i%2 == 0:
+            dp.append(dp[i-2]*dp[i-2]+dp[i-1])
+        else:
+            dp.append(2*dp[i-1]-1)
     print(dp[n-1]%10007)
