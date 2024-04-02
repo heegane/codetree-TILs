2f9f6e6f1b56fcf9f13da2108a0ea08bfeb27903
Lee Hyeonhee <dlgusgml1314@naver.com>
2024-04-02 16:31:17 +0900

    Title: 사각형 채우기 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240402/사각형 채우기 2/rectangle-fill-2.py
+++ 240402/사각형 채우기 2/rectangle-fill-2.py
@@ -1,13 +1,10 @@
 n = int(input())
 
-dp = [1,3,5]
+dp = [1, 3, 5]
 
 if n <= 3:
-    print(dp[n-1]%10007)
+    print(dp[n - 1] % 10007)
 else:
-    for i in range(3,n+1):
-        if i%2 == 0:
-            dp.append(dp[i-2]*dp[i-2]+dp[i-1])
-        else:
-            dp.append(2*dp[i-1]-1)
-    print(dp[n-1]%10007)
+    for i in range(3, n + 1):
+        dp.append(2 * dp[i - 2] + dp[i - 1])
+    print(dp[n - 1] % 10007)
