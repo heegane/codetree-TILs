e6e551b73504b79a4d77de45d9a14d9b0595616e
Lee Hyeonhee <dlgusgml1314@naver.com>
2024-05-09 17:39:55 +0900

    Title: 달에서 무게 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240509/달에서 무게 구하기/weight-on-the-moon.java
+++ 240509/달에서 무게 구하기/weight-on-the-moon.java
@@ -3,6 +3,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int a = 13,
         double b = 0.165;
-        System.out.printf("%d * %lf = %.6lf",a,b,a*b);
+        System.out.printf("%d * %f = %.6f",a,b,a*b);
     }
 }
